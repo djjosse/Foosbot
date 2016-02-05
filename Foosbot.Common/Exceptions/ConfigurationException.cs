b753aea8fa95beaf0de63e2a6d31494bcf61f05c
djjosse <gleyzer.yosi@gmail.com>
2016-02-05 20:39:01 +0200

    Configuration Exception

---
+++ Foosbot.Common/Exceptions/ConfigurationException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foosbot
+{
+    /// <summary>
+    /// Exception Thrown in case configuration error appeared
+    /// </summary>
+    public class ConfigurationException : Exception
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="message">Message</param>
+        public ConfigurationException(string message) : base(message) { }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="message">Message</param>
+        /// <param name="innerException">Inner Exception</param>
+        public ConfigurationException(string message, Exception innerException) 
+            : base(message, innerException) { }
+    }
+}
